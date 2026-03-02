7cee3b01e9c75ddc916b69e1463cd00ff3a23a2b
brunomadrigal0 <tobias_klein1004@outlook.com>
2026-03-02 07:24:48 -0500

    update function

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import GammaAPI from 'polymarket-provider';
+import GammaAPI from 'odds-analyzer';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
